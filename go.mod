b7971ade969b767c39480e9cdf13dddc87cb9de6
Sarim Abbas <sarim.stays@gmail.com>
2020-05-01 23:46:51 -0400

    init

---
+++ go.mod
@@ -0,0 +1,9 @@
+module boop_api
+
+go 1.14
+
+require (
+	github.com/jinzhu/gorm v1.9.12 // indirect
+	github.com/labstack/echo/v4 v4.1.16
+	github.com/lib/pq v1.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"encoding/json"
+	"github.com/jinzhu/gorm/dialects/postgres"
+	"github.com/labstack/echo/v4"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	// 	e.POST("/users", saveUser)
+	// e.GET("/users/:id", getUser)
+	// e.PUT("/users/:id", updateUser)
+	// e.DELETE("/users/:id", deleteUser)
+
+	e.Logger.Fatal(e.Start(":1323"))
+}
